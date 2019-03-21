64cf0ba11bdaca14060fe5043c5fea9bef5cbb78
Henrik Waardahl <Hwaa@kth.se>
2019-03-21 14:02:28 +0100

    new app lets go

---
+++ build/precache-manifest.829149d1b120b92bedd5c6e2bae91449.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cf6b0de12edc42df4476",
+    "url": "/static/js/main.54217518.chunk.js"
+  },
+  {
+    "revision": "788166b242a70786d5dd",
+    "url": "/static/js/2.1359d322.chunk.js"
+  },
+  {
+    "revision": "cf6b0de12edc42df4476",
+    "url": "/static/css/main.8af8c63a.chunk.css"
+  },
+  {
+    "revision": "11f899806247981e7352337052beef0f",
+    "url": "/index.html"
+  }
+];
---
+++ src/server/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = module.exports.io = require('socket.io')(server)
+
+const PORT = process.env.PORT || 3231 
+
+const SocketManager = require('./SocketManager')
+
+app.use( express.static(__dirname + '/../../build'))
+io.on('connection', SocketManager)
+
+app.listen(PORT, ()=>{
+	console.log("Connected to port:" + PORT);
+})
